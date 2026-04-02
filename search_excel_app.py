ebb84339bc2189f75a0c62ac405412952162e448
kuzo96 <kuzo9601@gmail.com>
2026-04-02 08:15:20 +0000

    Test02

--- search_excel_app.py
+++ search_excel_app.py
@@ -21,7 +21,7 @@ section.main > div {
 }
 </style>
 """, unsafe_allow_html=True)
-#Test
+#Test2
 # ================== DATA DIR ==================
 BASE_DATA_DIR = os.environ.get("DATA_DIR", "/opt/streamlit-data")
 
